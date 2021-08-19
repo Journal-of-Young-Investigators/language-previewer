7840905d33a096217c3d183ef93eb2dc7265fbaa
U-DESKTOP-OJ6PD5N\kubiv <jakub.vogel@jyi.org>
2021-08-19 13:17:44 -0400

    Finished implementing the barebones structure of the app. Played around tons with CSS here to get it look decent and sort of like how I envisioned.

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,23 @@ class inputArticleContentFormat {
   body = '';
 };
 
+interface htmlFormat {
+  __html: string
+}
+
 type MyProps = {
 
 }
 
 type MyState = {
-  article: inputArticleContentFormat;
+  article: inputArticleContentFormat,
+  formattedHTML: htmlFormat
 }
 
 class LanguagePreviewer extends React.Component <MyProps, MyState> {
   state: MyState = {
-    article: new inputArticleContentFormat()
+    article: new inputArticleContentFormat(),
+    formattedHTML: {__html: ''}
   }
   
   handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -34,22 +40,57 @@ class LanguagePreviewer extends React.Component <MyProps, MyState> {
     }));
   }
 
+  handleSubmit = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    event.preventDefault();
+    this.setState({
+      formattedHTML: {__html: '<h1>This is a test</h1>'}
+    });
+  }
+
+  preview = () => {
+    return this.state.formattedHTML;
+  }
+
   render() {
     return (
-      <div className="container">
+      <div>
         <div className="input-area">
           <form>
-            <div className="input-item">Language
+            <div className="input-item">
+              <label htmlFor="langCode">Language</label>
               <input name="langCode" type="text" value={this.state.article.langCode} onChange={this.handleChange}/>
             </div>
-
+            <div className="input-item">
+              <label htmlFor="title">Title</label>
+              <input name="title" type="text" value={this.state.article.title} onChange={this.handleChange}/>
+            </div>
+            <div className="input-item">
+              <label htmlFor="date">Date</label>
+              <input name="date" type="text" value={this.state.article.date} onChange={this.handleChange}/>
+            </div>
+            <div className="input-item">
+              <label htmlFor="authors">Authors</label>
+              <input name="authors" type="text" value={this.state.article.authors} onChange={this.handleChange}/>
+            </div>
+            <div className="input-item">
+              <label htmlFor="translators">Translators</label>
+              <input name="translators" type="text" value={this.state.article.translators} onChange={this.handleChange}/>
+            </div>
+            <div className="input-item">
+              <label htmlFor="body">Body</label>
+              <input name="body" type="text" value={this.state.article.body} onChange={this.handleChange}/>
+            </div>
+            <div className="input-item">
+              <div className="button">
+                <button type="submit" onClick={this.handleSubmit}>Show Preview</button>
+              </div>
+            </div>
           </form>
+        </div>
+
+        <div className="output-area">
+          <div className="output" dangerouslySetInnerHTML={this.preview()}></div>
         </div>  
-
-
-
-
-
       </div>
     )
   }
