70577d46110d06b2f72bcac14789b29ffc916b10
U-DESKTOP-OJ6PD5N\kubiv <jakub.vogel@jyi.org>
2021-08-19 23:32:48 -0400

    Add more CSS styling to display some of the form elements as rows. Add logic to handle the inputs and render them as HTML.

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ type MyState = {
 class LanguagePreviewer extends React.Component <MyProps, MyState> {
   state: MyState = {
     article: new inputArticleContentFormat(),
-    formattedHTML: {__html: ''}
+    formattedHTML: {__html: '<em>Input text to the left and click show preview</em>'}
   }
   
   handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -40,10 +40,42 @@ class LanguagePreviewer extends React.Component <MyProps, MyState> {
     }));
   }
 
+  handleTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState(prevState => ({
+      article: {
+        ...prevState.article,
+        [name]: value
+      }
+    }));
+  }
+
+  format = () => {
+    let data = '<h2>' + this.state.article.title + '</h2>';
+    data += '<div className="data">' + this.state.article.date + '</div>';
+    data += '<h3>' + this.formatAuthorsOrTranslators(this.state.article.authors.split(',')) + '</h3>';
+    data += '<h3>' + this.formatAuthorsOrTranslators(this.state.article.translators.split(',')) +'</h3>';
+    data += '<p>' + this.state.article.body + '</p>';
+    
+    return data;
+  }
+
+  formatAuthorsOrTranslators = (names: string[]) => {
+    let formatted = '';
+    for (let i = 0; i < names.length; i++) {
+      formatted += names[i];
+      if (i !== names.length-1) formatted += ', ';
+    }
+    
+    return formatted;
+  }
+
   handleSubmit = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     event.preventDefault();
+    const data = this.format();
     this.setState({
-      formattedHTML: {__html: '<h1>This is a test</h1>'}
+      formattedHTML: {__html: data}
     });
   }
 
@@ -55,22 +87,25 @@ class LanguagePreviewer extends React.Component <MyProps, MyState> {
     return (
       <div>
         <div className="input-area">
-          <form>
-            <div className="input-item">
-              <label htmlFor="langCode">Language</label>
-              <input name="langCode" type="text" value={this.state.article.langCode} onChange={this.handleChange}/>
+            <div className="input-row">
+              <div className="input-item">
+                <label htmlFor="langCode">Language</label>
+                <input name="langCode" type="text" value={this.state.article.langCode} onChange={this.handleChange}/>
+              </div>
+              <div className="input-item">
+                <label htmlFor="title">Title</label>
+                <input name="title" type="text" value={this.state.article.title} onChange={this.handleChange}/>
+              </div>
             </div>
-            <div className="input-item">
-              <label htmlFor="title">Title</label>
-              <input name="title" type="text" value={this.state.article.title} onChange={this.handleChange}/>
-            </div>
-            <div className="input-item">
-              <label htmlFor="date">Date</label>
-              <input name="date" type="text" value={this.state.article.date} onChange={this.handleChange}/>
-            </div>
-            <div className="input-item">
-              <label htmlFor="authors">Authors</label>
-              <input name="authors" type="text" value={this.state.article.authors} onChange={this.handleChange}/>
+            <div className="input-row">
+              <div className="input-item">
+                <label htmlFor="date">Date</label>
+                <input name="date" type="text" value={this.state.article.date} onChange={this.handleChange}/>
+              </div>
+              <div className="input-item">
+                <label htmlFor="authors">Authors</label>
+                <input name="authors" type="text" value={this.state.article.authors} onChange={this.handleChange}/>
+              </div>
             </div>
             <div className="input-item">
               <label htmlFor="translators">Translators</label>
@@ -78,14 +113,13 @@ class LanguagePreviewer extends React.Component <MyProps, MyState> {
             </div>
             <div className="input-item">
               <label htmlFor="body">Body</label>
-              <input name="body" type="text" value={this.state.article.body} onChange={this.handleChange}/>
+              <textarea className="body" name="body" value={this.state.article.body} onChange={this.handleTextChange}></textarea>
             </div>
             <div className="input-item">
               <div className="button">
                 <button type="submit" onClick={this.handleSubmit}>Show Preview</button>
               </div>
             </div>
-          </form>
         </div>
 
         <div className="output-area">
