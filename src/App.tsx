6b8e3d9031c8529359e94e8fc6c3ab7833edb1e9
U-DESKTOP-OJ6PD5N\kubiv <jakub.vogel@jyi.org>
2021-08-23 11:46:03 -0400

    Use my previously created LanguageSwitcher to preview the article.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
 import React from 'react';
 import './App.css';
+import {LanguageSwitcher} from './components/lang-switcher'
+
+interface jyiArticleContent {
+  // ISO 639 code to identify language; 'en' for English, 'es' for Spanish, etc.
+  // See list here: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+  // You don't have to store display name of language ('English' for 'en' for example), get it using this: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DisplayNames/DisplayNames
+  langCode: keyof typeof Language;
+  title: string;
+  date: string;
+  authors: string[];
+  translators?: string[];
+  body: string;
+};
+
+interface jyiArticle {
+  id: string;
+  content: jyiArticleContent[];
+};
+
+enum Language {
+  en='English',
+  es='Español',
+  fr='Français',
+  zh='中國人'
+};
 
 class inputArticleContentFormat {
   langCode = '';
@@ -10,31 +35,32 @@ class inputArticleContentFormat {
   body = '';
 };
 
-interface htmlFormat {
-  __html: string
-}
-
 type MyProps = {
 
 }
 
 type MyState = {
-  article: inputArticleContentFormat,
-  formattedHTML: htmlFormat
+  inputArticle: inputArticleContentFormat;
+  previewArticle: jyiArticle;
+  showPreview: boolean;
 }
 
 class LanguagePreviewer extends React.Component <MyProps, MyState> {
   state: MyState = {
-    article: new inputArticleContentFormat(),
-    formattedHTML: {__html: '<em>Input text to the left and click show preview</em>'}
+    inputArticle: new inputArticleContentFormat(),
+    previewArticle: {
+      id: '1',
+      content: []
+    },
+    showPreview: false
   }
   
   handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const name = event.target.name;
     const value = event.target.value;
     this.setState(prevState => ({
-      article: {
-        ...prevState.article,
+      inputArticle: {
+        ...prevState.inputArticle,
         [name]: value
       }
     }));
@@ -44,39 +70,36 @@ class LanguagePreviewer extends React.Component <MyProps, MyState> {
     const name = event.target.name;
     const value = event.target.value;
     this.setState(prevState => ({
-      article: {
-        ...prevState.article,
+      inputArticle: {
+        ...prevState.inputArticle,
         [name]: value
       }
     }));
   }
 
-  format = () => {
-    let data = '<h2>' + this.state.article.title + '</h2>';
-    data += '<div class="date">' + this.state.article.date + '</div>';
-    data += '<h3>' + this.formatAuthorsOrTranslators(this.state.article.authors.split(',')) + '</h3>';
-    data += '<h3>' + this.formatAuthorsOrTranslators(this.state.article.translators.split(',')) +'</h3>';
-    data += '<p>' + this.state.article.body + '</p>';
-    
-    return data;
-  }
-
-  formatAuthorsOrTranslators = (names: string[]) => {
-    let formatted = '';
-    for (let i = 0; i < names.length; i++) {
-      formatted += names[i];
-      if (i !== names.length-1) formatted += ', ';
-    }
-    
-    return formatted;
-  }
-
-  handleSubmit = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  preview = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     event.preventDefault();
-    const data = this.format();
-    this.setState({
-      formattedHTML: {__html: data}
-    });
+    const inputAuthors = this.state.inputArticle.authors.split(','); 
+    const inputTranslators = this.state.inputArticle.translators.split(',');
+
+    const newArticleContent: jyiArticleContent = {
+      langCode: this.state.inputArticle.langCode as keyof typeof Language,
+      title: this.state.inputArticle.title,
+      date: this.state.inputArticle.date,
+      authors: inputAuthors,
+      translators: inputTranslators,
+      body: this.state.inputArticle.body
+    }
+
+    const added = this.state.previewArticle.content.concat(newArticleContent);
+    
+    this.setState(prevState => ({
+      previewArticle: {
+        ...prevState.previewArticle,
+        content: added
+      },
+      showPreview: true
+    }));
   }
 
   render() {
@@ -86,40 +109,43 @@ class LanguagePreviewer extends React.Component <MyProps, MyState> {
             <div className="input-row">
               <div className="input-item">
                 <label htmlFor="langCode">Language</label>
-                <input name="langCode" type="text" placeholder="'en' for English..." value={this.state.article.langCode} onChange={this.handleChange}/>
+                <input name="langCode" type="text" placeholder="'en' for English..." value={this.state.inputArticle.langCode} onChange={this.handleChange}/>
               </div>
               <div className="input-item">
                 <label htmlFor="title">Title</label>
-                <input name="title" type="text" value={this.state.article.title} onChange={this.handleChange}/>
+                <input name="title" type="text" value={this.state.inputArticle.title} onChange={this.handleChange}/>
               </div>
             </div>
             <div className="input-row">
               <div className="input-item">
                 <label htmlFor="date">Date</label>
-                <input name="date" type="text" placeholder="1 January 1990..." value={this.state.article.date} onChange={this.handleChange}/>
+                <input name="date" type="text" placeholder="1 January 1990..." value={this.state.inputArticle.date} onChange={this.handleChange}/>
               </div>
               <div className="input-item">
                 <label htmlFor="authors">Authors</label>
-                <input name="authors" type="text" value={this.state.article.authors} onChange={this.handleChange}/>
+                <input name="authors" type="text" value={this.state.inputArticle.authors} onChange={this.handleChange}/>
               </div>
             </div>
             <div className="input-item">
               <label htmlFor="translators">Translators</label>
-              <input name="translators" type="text" value={this.state.article.translators} onChange={this.handleChange}/>
+              <input name="translators" type="text" value={this.state.inputArticle.translators} onChange={this.handleChange}/>
             </div>
             <div className="input-item">
               <label htmlFor="body">Body</label>
-              <textarea className="body" name="body" value={this.state.article.body} onChange={this.handleTextChange}></textarea>
+              <textarea className="text-area" name="body" value={this.state.inputArticle.body} onChange={this.handleTextChange}></textarea>
             </div>
             <div className="input-item">
               <div className="button">
-                <button type="submit" onClick={this.handleSubmit}>Show Preview</button>
+                <button type="submit" onClick={this.preview}>Show Preview</button>
               </div>
             </div>
         </div>
 
         <div className="output-area">
-          <div className="output" dangerouslySetInnerHTML={this.state.formattedHTML}></div>
+          {
+            this.state.showPreview &&
+            <LanguageSwitcher article={this.state.previewArticle}/>
+          }
         </div>  
       </div>
     )
